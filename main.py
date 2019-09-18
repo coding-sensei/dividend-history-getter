7aa275b71f780b358e1a16b1fe2f6d26907bfb3d
coding sensei <investing.sensei.2019@gmail.com>
2019-09-18 15:05:43 -0500

    refactor configuration of webdriver out to function

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from lxml.html import fromstring
 import requests
 from itertools import cycle
 from fake_useragent import UserAgent
+from bs4 import BeautifulSoup
 
 from selenium.webdriver.common.proxy import Proxy, ProxyType
 
@@ -20,34 +21,26 @@ def get_proxies():
 
 def get_page_content(url, proxy_pool):
     print(next(proxy_pool))
-    pass
 
-#If you are copy pasting proxy ips, put in the list below
-#proxies = ['121.129.127.209:80', '124.41.215.238:45169', '185.93.3.123:8080', '194.182.64.67:3128', '106.0.38.174:8080', '163.172.175.210:3128', '13.92.196.150:8080']
+def get_configured_webdriver(proxy):
+    ua = UserAgent()
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--proxy-server={}'.format(proxy))
+    chrome_options.add_argument('user-agent={}'.format(ua.random))
+    chrome_options.add_argument('--no-sandbox') # required when running as root user. otherwise you would get no sandbox errors.
+    driver = webdriver.Chrome(options=chrome_options,
+          service_args=['--verbose', '--log-path=/tmp/chromedriver.log'])
+    return driver
+
+
 proxy_list = get_proxies()
 proxy_pool = cycle(proxy_list)
 
-ua = UserAgent()
-#print(ua.random)
-#print(ua.random)
-#print(ua.random)
-print(next(proxy_pool))
-print(next(proxy_pool))
-print(next(proxy_pool))
 proxy = next(proxy_pool)
-proxies = {"http": proxy, "https": proxy}
 
-# Option 1 - with ChromeOptions
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument('--headless')
-chrome_options.add_argument('--proxy-server={}'.format(proxy))
-chrome_options.add_argument('user-agent={}'.format(ua.random))
-chrome_options.add_argument('--no-sandbox') # required when running as root user. otherwise you would get no sandbox errors.
-driver = webdriver.Chrome(options=chrome_options,
-      service_args=['--verbose', '--log-path=/tmp/chromedriver.log'])
+driver = get_configured_webdriver(proxy)
 
-# Log path added via service_args to see errors if something goes wrong (always a good idea - many of the errors I encountered were described in the logs)
-# And now you can add your website / app testing functionality:
 url = 'https://python.org'
 #url = 'https://seekingalpha.com/symbol/COST/dividends/history'
 driver.get(url)
@@ -64,6 +57,10 @@ else:
 
 print(proxy_list)
 t = get_page_content(url, proxy_pool)
+
+#soup=BeautifulSoup(browser.page_source)
+
+
 #with open("web.html", "w") as text_file:
 #      text_file.write(driver.page_source)
 
@@ -71,22 +68,6 @@ t = get_page_content(url, proxy_pool)
 
 #from selenium import webdriver
 #from bs4 import BeautifulSoup
-#
-#chrome_options = Options()
-#chrome_options.add_argument("--headless")
-#chrome_options.add_argument("--window-size=1920x1080")
-#chrome_options.add_argument("--disable-dev-shm-usage")
-#chrome_options.add_argument("--no-sandbox")
-#chrome_options.add_argument('--disable-gpu')
-#
-#driver = webdriver.Chrome(options=chrome_options)
-#
-#
-#
-#browser.get('http://webpage.com')
-#
-#
-#
 #
 #
 #soup=BeautifulSoup(browser.page_source)
