343525918f55cd26cb08d1bc3d2517982ddf3e22
coding sensei <investing.sensei.2019@gmail.com>
2019-09-18 09:45:10 -0500

    add get random proxy and random user-agent

--- Dockerfile
+++ Dockerfile
@@ -2,3 +2,5 @@ FROM joyzoursky/python-chromedriver
 
 COPY requirements.txt /tmp/requirements.txt
 RUN pip install -r /tmp/requirements.txt && rm /tmp/requirements.txt
+
+ENTRYPOINT /bin/bash
--- main.py
+++ main.py
@@ -19,3 +19,54 @@ browser.get('http://webpage.com')
 
 
 soup=BeautifulSoup(browser.page_source)
+
+
+#from lxml.html import fromstring
+#import requests
+#from itertools import cycle
+#from fake_useragent import UserAgent
+#
+#
+#def get_proxies():
+#    url = 'https://free-proxy-list.net/'
+#    response = requests.get(url)
+#    parser = fromstring(response.text)
+#    proxies = set()
+#    for i in parser.xpath('//tbody/tr')[:10]:
+#        if i.xpath('.//td[7][contains(text(),"yes")]'):
+#            proxy = ":".join([i.xpath('.//td[1]/text()')[0], i.xpath('.//td[2]/text()')[0]])
+#            proxies.add(proxy)
+#    return proxies
+#
+#
+##If you are copy pasting proxy ips, put in the list below
+##proxies = ['121.129.127.209:80', '124.41.215.238:45169', '185.93.3.123:8080', '194.182.64.67:3128', '106.0.38.174:8080', '163.172.175.210:3128', '13.92.196.150:8080']
+#proxies = get_proxies()
+#proxy_pool = cycle(proxies)
+#
+#ua = UserAgent()
+#print(ua.random)
+#print(ua.random)
+#print(ua.random)
+#print(next(proxy_pool))
+#print(next(proxy_pool))
+#print(next(proxy_pool))
+#proxy = next(proxy_pool)
+#proxies = {"http": proxy, "https": proxy}
+#
+#headers = {
+#'User-Agent': ua.random }
+##headers = {
+##'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.13; #rv:60.0) Gecko/20100101 Firefox/60.0'}
+#
+#print(headers)
+#print(proxies)
+#url = 'https://www.streetinsider.com/dividend_history.php?q=cost'
+#
+#
+## Collect first page of artists’ list
+#html_content = requests.get(url, headers=headers, proxies=proxies)
+#
+#print(html_content.text)
+#
+#print(html_content.status_code)
